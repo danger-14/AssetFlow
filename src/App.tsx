a78fbbb667bf8791858d7d69129d1f96194c2b50
danger-14 <dan.gerald.cruz@wolt.com>
2026-07-20 15:14:32 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Inventory from "./pages/Inventory/Inventory";
-import AddSingleAsset from "./pages/AddSingleAsset/AddSingleAsset";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Inventory />} />
+        <Route path="/" element={<Navigate to="/inventory" replace />} />
         <Route path="/inventory" element={<Inventory />} />
-        <Route path="/inventory/add" element={<AddSingleAsset />} />
       </Routes>
     </BrowserRouter>
   );
