4e21a18ada7f8a6e082955c99bfcc166757a8c89
danger-14 <dan.gerald.cruz@wolt.com>
2026-07-20 15:28:44 +0300

    Add route for AssignDevice component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Inventory from "./pages/Inventory/Inventory";
 import AddSingleAsset from "./pages/AddSingleAsset/AddSingleAsset";
 import ImportShipment from "./pages/ImportShipment/ImportShipment";
+import AssignDevice from "./pages/AssignDevice/AssignDevice";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/inventory" element={<Inventory />} />
         <Route path="/inventory/add" element={<AddSingleAsset />} />
         <Route path="/inventory/import" element={<ImportShipment />} />
+        <Route path="/assign" element={<AssignDevice />} />
       </Routes>
     </BrowserRouter>
   );
