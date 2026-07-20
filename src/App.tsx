39f9a8afd23d80210c9634c18a96965ae7a6056d
danger-14 <dan.gerald.cruz@wolt.com>
2026-07-20 14:40:29 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Inventory from "./pages/Inventory/Inventory";
+import AddSingleAsset from "./pages/AddSingleAsset/AddSingleAsset";
 
 function App() {
-  return <Inventory />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inventory />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/inventory/add" element={<AddSingleAsset />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
